180a4a52ff2eb7ffc722dcf1dc198cec86f56250
Souhail Merroun <souhail.merroun1996@gmail.com>
2024-10-22 12:45:28 +0200

    replace with gatsby

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const Header = ({ siteTitle }) => (
+  <header
+    style={{
+      margin: `0 auto`,
+      padding: `var(--space-4) var(--size-gutter)`,
+      display: `flex`,
+      alignItems: `center`,
+      justifyContent: `space-between`,
+    }}
+  >
+    <ul>
+      <li>Menu 1</li>
+    </ul>
+  </header>
+)
+
+export default Header
